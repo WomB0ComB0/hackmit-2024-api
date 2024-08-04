34b11a03281d6063f4a8af98be211c5aa60fc6c4
Mike Odnis <airwolf635@gmail.com>
2024-08-04 12:40:37 -0400

    chore: removed depracated

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -1,40 +1,11 @@
 import { describe, expect, it } from "vitest";
-import { Hono, Context, MiddlewareHandler } from "hono";
-import { rateLimit, RateLimitBinding } from "../src/utils/limit";
+import { Hono, MiddlewareHandler } from "hono";
 import Scraper from '../src/classes/scraper'
 
-type keyConfig = {
-	key: string;
-};
-
-class PassingRateLimiter implements RateLimitBinding {
-	async limit(_: keyConfig) {
-		return { success: true };
-	}
-}
-
-class FailingRateLimiter implements RateLimitBinding {
-	async limit(_: keyConfig) {
-		return { success: false };
-	}
-}
-
-const passingRateLimiter: MiddlewareHandler = async (c, next) => {
-	return await rateLimit(new PassingRateLimiter(), () => {
-		return "someKey";
-	})(c, next);
-};
-
-const failingRateLimiter: MiddlewareHandler = async (c, next) => {
-	return await rateLimit(new FailingRateLimiter(), () => {
-		return "someKey";
-	})(c, next);
-};
 
 describe("API", () => {
   const app = new Hono();
 
-  app.use("*", passingRateLimiter);
   app.get("/api", async (c) => {
     return c.text("Hello from tRPC!");
   });
@@ -87,15 +58,5 @@ describe("API", () => {
     } catch (error) {
       console.error(error);
     }
-  });
-
-  it("Should be rate limited", async () => {
-    const res = await app.request("/api");
-    expect(res.status).toBe(429);
-  });
-
-  it("Should not be rate limited", async () => {
-    const res = await app.request("/api");
-    expect(res.status).toBe(200);
   });
 });
