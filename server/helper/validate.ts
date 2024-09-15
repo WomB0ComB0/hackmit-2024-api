473dd68891f7d1dad1d8a626fd4a8766b84348e2
Mike Odnis <airwolf635@gmail.com>
2024-09-15 07:56:59 -0400

    chore: started the controller

---
+++ server/helper/validate.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Request, Response } from "express";
+import { AnyZodObject } from "zod";
+
+const validate =
+  (schema: AnyZodObject) =>
+  asyc
---
+++ server/repository/CustomerRepo.ts
@@ -0,0 +1,19 @@
+class CustomerRepo {
+    async createCustomer() {
+        // Create logic here
+    }
+
+    async getCustomer() {
+        // Fetch logic here
+    }
+
+    async updateCustomer() {
+        // Update logic here
+    }
+
+    async deleteCustomer() {
+        // Delete logic here
+    }
+}
+
+export { CustomerRepo };
