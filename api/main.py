c758b2d6017a087ec52bf535c770538b94792282
Mike Odnis <airwolf635@gmail.com>
2024-09-17 22:32:48 -0400

    chore: file structure and folder structure

---
+++ api/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.routers import transactions
+
+app = FastAPI()
+
+app.include_router(transactions.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
